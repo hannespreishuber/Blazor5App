944afa9d37228beab9c2dd2bb1096afc67455b38
Hannes Preishuber <hannesp@ppedv.de>
2020-09-16 19:51:04 +0200

    demo

---
+++ Blazor5App/Data/WeatherForecast.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Blazor5App.Data
+{
+    public class WeatherForecast
+    {
+        public int Counter { get; set; }
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
---
+++ Blazor5App/Data/WeatherForecastService.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor5App.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            var rng = new Random();
+            var counter = 0;
+            return Task.FromResult(Enumerable.Range(1, 500).Select(index => new WeatherForecast
+            {
+                Counter=index,
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
